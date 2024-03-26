25f8bb68b9c2fc47a8b361488524af0f8dadb9a9
Ayhan Sipahi <info@ayhansipahi.net>
2024-03-26 17:58:37 +0100

    replace ducument selector with useref

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import styles from "./page.module.css";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import Quagga from 'quagga';
 import {useRouter} from 'next/navigation'
 
@@ -8,46 +8,50 @@ import {useRouter} from 'next/navigation'
 
 
 export default function Home() {
+    const okuyucuRef = useRef(null)
     const router = useRouter()
     const [barcode, setBarcode] = useState('')
 
     useEffect(()=>{
-        Quagga.init({
-            inputStream: {
-                name: "Live",
-                type: "LiveStream",
-                target: document.querySelector("#barcode-scanner"),
-                constraints: {
-                    width: 640,
-                    height: 480,
-                    facingMode: "environment"
+        if(okuyucuRef.current) {
+
+            Quagga.init({
+                inputStream: {
+                    name: "Live",
+                    type: "LiveStream",
+                    target: okuyucuRef.current,
+                    constraints: {
+                        width: 640,
+                        height: 480,
+                        facingMode: "environment"
+                    }
+                },
+                decoder: {
+                    readers: ["ean_reader"]
                 }
-            },
-            decoder: {
-                readers: ["ean_reader"]
-            }
-        }, function(err) {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.log("QuaggaJS çalışıyor.");
-            Quagga.start();
-        });
+            }, function (err) {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                console.log("QuaggaJS çalışıyor.");
+                Quagga.start();
+            });
 
-        Quagga.onDetected((result)=>{
-            console.log(result)
-            router.push('/product/' + result.codeResult.code )
-            Quagga.stop()
-        })
+            Quagga.onDetected((result) => {
+                console.log(result)
+                router.push('/product/' + result.codeResult.code)
+                Quagga.stop()
+            })
 
+        }
         return ()=>{
             Quagga.stop()
         }
-    },[router])
+    },[router, okuyucuRef])
   return (
       <main className={styles.main}>
-          <div id="barcode-scanner"></div>
+          <div ref={okuyucuRef}></div>
       </main>
   );
 }
