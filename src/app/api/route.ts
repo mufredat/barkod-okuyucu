3555cbb748a73678de0539c0340cb3ef98d72ba0
Ayhan Sipahi <info@ayhansipahi.net>
2024-04-15 16:56:08 +0200

    add kv storage

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,4 +1,5 @@
 import openfoodfacts from 'openfoodfacts-nodejs'
+import { kv } from '@vercel/kv'
 
 export async function GET(req:Request) {
     const { searchParams } = new URL(req.url)
@@ -6,9 +7,18 @@ export async function GET(req:Request) {
     if (!barkod) {
         return Response.json({ error: 'Missing barkod parameter' })
     }
-    // @ts-ignore
-    const client =  new openfoodfacts();
-    const product = await client.getProduct(barkod)
+    let product;
+
+    const vsRes = await kv.get(barkod)
+
+    if (vsRes) {
+        product = vsRes
+    }  else {
+        // @ts-ignore
+        const client =  new openfoodfacts();
+        product = await client.getProduct(barkod)
+        await kv.set(barkod, product)
+    }
 
     return Response.json({ product })
 }
